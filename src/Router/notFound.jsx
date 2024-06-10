a4cb54a15c0e19b9c51f7ac80bacede241afc572
Samuel Sánchez <samusanchezdr@gmail.com>
2024-06-10 13:17:40 +0200

    Router

---
+++ src/Router/notFound.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class NotFound extends Component{
+    render(){
+        return(
+            <h1>NO SE A ENCONTRADO LA RUTA</h1>
+        )
+    }
+}
+
+export default NotFound;
